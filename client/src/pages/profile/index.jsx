5d221df733c62e44ea58614077cfb8c6c889b833
Akash <chakash793@gmail.com>
2025-03-26 02:36:39 +0530

    Added profile setup feature

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -3,8 +3,10 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IoArrowBack} from "react-icons/io5"
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { getColor } from "@/lib/utils";
-
+import { colors, getColor } from "@/lib/utils";
+import { FaPlus, FaTrash } from "react-icons/fa";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 const Profile = () => {
     const navigate= useNavigate();
     const {userInfo} = useAppStore();
@@ -33,10 +35,60 @@ const Profile = () => {
                   </div>
                 )}
               </Avatar>
+              {
+                hovered && (
+                  <div className="absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full">
+                  {
+                    image ? <FaTrash className="text-white text-3xl cursor-pointer"/> : <FaPlus />
+                  }
+                  </div>
+                )
+              }
                 </div>
-                
+                <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center">
+                  <div className="w-full">
+                    <Input 
+                    placeholder="Email" 
+                    type="email" 
+                    disabled value={userInfo.email}
+                    className="rounded-lg p-6 bg-[#2c2e3b] border-0"/>
+                  </div>
+                  <div className="w-full">
+                    <Input 
+                    placeholder="First Name" 
+                    type="text" 
+                    onChange = {(e)=> setFirstName(e.target.value)}
+                    value={firstName} 
+                    className="rounded-lg p-6 bg-[#2c2e3b] border-0"/>
+                  </div>
+                  <div className="w-full">
+                    <Input 
+                    placeholder="Last Name" 
+                    type="text" 
+                    onChange = {(e)=> setLastName(e.target.value)}
+                    value={lastName} 
+                    className="rounded-lg p-6 bg-[#2c2e3b] border-0"/>
+                  </div>
+                  <div className="w-full flex gap-5">
+                           {colors.map((color, index) => (
+                           <div
+                              key={index}
+                            className={`${color} h-8 w-8 rounded-full cursor-pointer transition-all duration-300
+                            ${selectedColor === index ? "ring-2 ring-white" : ""}
+                            `} 
+                            onClick={()=>setSelectedColor(index)}
+                    ></div>
+                      ))}
+                    </div>
+               </div>
             </div>
+
+            <div className="w-full">
+          <Button className="h-16 w-full bg-purple-700 hover:bg-purple-900 transition-all duration-300" onClick={saveChanges}>Save Changes</Button>
         </div>
+        </div>
+        
+
     </div>
 }
 
